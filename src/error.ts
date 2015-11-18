d38df3636096077417fe927435ae75f5f4c5e09d
Lawrence Cheung <lawrence0819@gmail.com>
2015-11-18 18:26:56 +0800

    Update error handler

--- src/error.ts
+++ src/error.ts
@@ -2,14 +2,13 @@ export function * handler(next) {
   try {
     yield next;
   } catch (err) {
-    if (typeof err === 'string') {
-      this.status = 500;
-      this.body = err;
-      this.app.emit('error', new Error(err), this);
-    } else {
-      this.status = err.status || 500;
-      this.body = err.message;
-      this.app.emit('error', err, this);
-    }
+    this.status = err.status || 500;
+    this.body = {
+      error: {
+        status: this.status,
+        message: err.message
+      }
+    };
+    this.app.emit('error', err, this);
   }
 };
